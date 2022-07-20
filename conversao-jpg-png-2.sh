5df7896ccd1def000f36b3b9149ce28584eebccd
bbranco <bbranco@riachuelo.com.br>
2022-07-20 15:43:56 -0300

    add job e altera script

---
+++ conversao-jpg-png-2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+directory=/Users/barbara/CursoShell/Scripts
+cd $directory/imagens-livros
+LIST=$(ls -la | awk '{print $9}')
+
+echo "Inicializando job"
+if [ ! -e "$directory/imagens-livros-png" ]; then
+    echo "[*] Criando pasta"
+    mkdir $directory/imagens-livros-png
+fi
+echo "[*] Pasta já existe"
+if [ -e $directory/imagens-livros-png/$1.png ]; then
+    echo "Arquivos já existe"
+    exit 0
+else
+    echo "[*] Convertendo arquivo"
+    convert $directory/imagens-livros/$1.jpg $directory/imagens-livros-png/$1.png
+fi
+echo "[*] Finalizando conversão"
--- conversar-jpg-png.sh
+++ conversar-jpg-png.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-cd /Users/barbara/CursoShell/Scripts/imagens-livros
+directory=/Users/barbara/CursoShell/Scripts
+cd $directory/imagens-livros
 LIST=$(ls -la | awk '{print $9}')
-extensao=".jpg"
-
-echo $extensao
-
-# for VAR in $LIST
-# do
-#     echo REALIZANDO A CONVERSÃO DO ARQUIVO: $VAR
-# done
-
-VAR="teste"
-echo $VAR
-
+ls -la $directory
+if [ ! -e $directory/imagens-livros-png ]; then
+    echo "[*] Criando pasta para arquivos PNG"
+    mkdir $directory/imagens-livros-png
+else
+    echo "[*] Pasta já existente para arquivos PNG"
+fi
+ls -la $directory
+echo "**************************************"
+echo "[*] Executando conversão dos arquivos"
+for VAR in $LIST; do
+    echo "Convertido: " $VAR
+    #convert $directory/imagens-livros/*.jpg $directory/imagens-livros-png/*.png
+done
+echo "[*] Conversão Finalizada"
+echo "**************************************"
+echo "[*] Pasta Base"
+ls $directory/imagens-livros
+echo "[*]  Pasta Conversão"
+ls $directory/imagens-livros-png
