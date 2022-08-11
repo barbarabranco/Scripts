c70385fb655ddbc053d81c036d97fc14b28b2922
bbranco <bbranco@riachuelo.com.br>
2022-08-10 23:03:01 -0300

    ajustes cv

--- conversao-novos-livros.sh
+++ conversao-novos-livros.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
 
+converte_imagem() {
+    local caminho_imagem=$1
+    imagem_sem_extensao=$(ls $caminho_imagem | awk -F. '{ print $1 }')
+    #convert $imagem_sem_extensao.jpg $imagem_sem_extensao.png
+    rm -rf $imagem_sem_extensao.png
+}
+
+varredura_diretorio() {
+    cd $1
+    for arquivo in *; do
+        local caminho_arquivo=$(find ~/CursoShell/Scripts/imagens-novos-livros/ -name $arquivo)
+        if [ -d $caminho_arquivo ]; then
+            varredura_diretorio $caminho_arquivo
+        else
+            converte_imagem $caminho_arquivo
+        fi
+    done
+}
+
+#INICIANDO SCRIPT
+echo "[*] Iniciando Conversão de imagens!"
+varredura_diretorio ~/CursoShell/Scripts/imagens-novos-livros
+if [ $? -eq 0 ]; then
+    echo "[*] Conversão realizada com sucesso!"
+else
+    echo "[*] Conversão com falha!"
+fi
