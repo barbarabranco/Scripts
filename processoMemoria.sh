d09f4cdbcf4e57c340e7e1e0a5597ba8cbefbc42
bbranco <bbranco@riachuelo.com.br>
2022-10-17 15:47:58 -0300

    add novos exercicios

--- processoMemoria.sh
+++ processoMemoria.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-processos=$(ps -e -o pid | head -n 11)
+
+processos=$(ps -e -o pid -m | head -n 11 | grep '[0-9]')
+
+for pid in $processos
+do
+    nome_processo=$(ps -p $pid -o ucomm=)
+    #comando que pega o nome do processo
+    echo -n $(date +%F, %H:%M:%S) >> $nome_processo.log
+    tamanho_processo=$(ps -p $pid -o vsize | grep '[0-9]')
+    #comando que converte o valor do pid em MB
+    echo "$(bc <<< "scale=2;$tamanho_processo/1024") MB" >> $nome_processo.log
+done
+
 
